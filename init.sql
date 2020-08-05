9fe7252938ab25595047e24e14b3b9fd125df01b
Mahoney96 <cmahoney966@gmail.com>
2020-08-05 01:13:07 -0700

    part 2 done

--- init.sql
+++ init.sql
@@ -41,12 +41,31 @@ SELECT * FROM person WHERE favorite_color = 'organge' OR favorite_color = 'green
 --#13
 SELECT * FROM person WHERE favorite_color IN ('orange', 'green', 'blue');
 
---#14
+--#id 14
 SELECT * FROM person WHERE favorite_color IN( 'yellow', 'purple');
 
 
 -- PART II --
 
+
+--#1
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY, person_id INTEGER, product_name VARCHAR(200), product_price NUMERIC, quantity INTEGER );
+
+--#2
+INSERT INTO orders ( person_id, product_name, product_price, quantity ) VALUES ( 0, 'Product', 12.50, 2);
+
+--#3
+SELECT * FROM orders;
+
+--#4
+SELECT SUM(product_price * quantity) FROM orders;
+
+--#5
+SELECT SUM(product_price * quantity) FROM orders;
+
+--#6
+SELECT SUM(product_price * quantity) FROM orders WHERE person_id = 0; 
 
 
 
